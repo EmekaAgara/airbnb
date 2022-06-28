bae15dc829b09df59e1f194569039d73044105f6
EmekaAgara <emekaagara@gmail.com>
2022-06-29 00:07:47 +0100

    banner done

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Banner from './Banner.js'
+
 
 function Home() {
   return (
     <div className='home'>
-        Home
+        <Banner/>
     </div>
   )
 }
