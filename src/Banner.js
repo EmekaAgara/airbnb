46552bdc56fec93e71fde6dee0bc7007abe1a2a2
EmekaAgara <emekaagara@gmail.com>
2022-06-30 16:35:49 +0100

    banner done

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,9 @@
 import React, {useState} from 'react'
 import './Banner.css'
 import { Button } from "@mui/material"
+import Search from './Search';
+
+
 
 function Banner() {
     const [showSearch, setShowSearch] = useState
@@ -9,9 +12,12 @@ function Banner() {
   return (
     <div className='banner'>
         <div className='banner__search'>
-            {showSearch && <h1>SHOW DATE PICKER</h1>}
-            <Button className='banner__searchButton' variant='outlined'>
-            Search Dates
+            {showSearch && <Search/>}
+            
+            <Button onClick={() => setShowSearch(!showSearch)}
+             className='banner__searchButton' variant='outlined'>
+                {showSearch ? "Hide" :
+                 "Search Dates"}
             </Button>
         </div>
 
