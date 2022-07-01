2fc4f1715cd75eae529e875adf15a5479c68fc15
EmekaAgara <emekaagara@gmail.com>
2022-07-01 15:02:02 +0100

    footer done

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <p>© 2022 Airbnb practice app by Agara ! No rights reserved - Just testing my skills</p>
+        <p>Privacy · Terms · Sitemap · Destinations</p>
+    </div>
+  )
+}
+
+export default Footer
