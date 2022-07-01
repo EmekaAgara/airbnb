b20ba90bc842abd34219757fb3dbcea1ac0c2eae
EmekaAgara <emekaagara@gmail.com>
2022-07-01 15:26:13 +0100

    routes done

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
-import { BrowserRoute as Router, Switch, Route, Link } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import SearchPage from './SearchPage';
 
 function App() {
   return (
     <div className="app">
       <Router>
         <Header />
-        <Home />
+
+        <Routes>
+
+          <Route path='/' element={<Home/>} />
+          <Route path='/search' element={<SearchPage/>} />
+        
+        </Routes>
+
         <Footer />
+
       </Router>
     </div>
   );
