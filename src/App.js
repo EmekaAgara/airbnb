d7df42ccc586fc2350bbbca5e7ded58a46e90faa
EmekaAgara <emekaagara@gmail.com>
2022-07-01 15:07:09 +0100

    homepage done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRoute as Router, Switch, Route, Link } from "react-router-dom"; 
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Home />
+        <Footer />
+      </Router>
     </div>
   );
 }
