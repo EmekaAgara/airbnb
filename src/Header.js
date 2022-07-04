3d618114a91dff1ba299aaa68721fbfdfb2f44a0
EmekaAgara <emekaagara@gmail.com>
2022-07-04 17:43:59 +0100

    all done

--- src/Header.js
+++ src/Header.js
@@ -4,26 +4,31 @@ import SearchIcon from '@mui/icons-material/Search'
 import LanguageIcon from '@mui/icons-material/Language'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { Avatar } from '@mui/material'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <div className='header'>
+
+      <Link to={'/'}>
         <img className='header__icon'
-            src='https://studiousguy.com/wp-content/uploads/2020/06/airbnb-logo.png'
-            alt='logo'
-        />
+          src='https://studiousguy.com/wp-content/uploads/2020/06/airbnb-logo.png'
+          alt='logo'
+        /> 
+      </Link>
+        
 
-        <div className='header__center'>
-            <input type="text" />
-            <SearchIcon/>
-        </div>
+      <div className='header__center'>
+        <input type="text" />
+        <SearchIcon/>
+      </div>
 
-        <div className='header__right'>
-            <p>Become a host</p>
-            <LanguageIcon/>
-            <ExpandMoreIcon/>
-            <Avatar/>
-        </div>
+      <div className='header__right'>
+        <p>Become a host</p>
+        <LanguageIcon/>
+        <ExpandMoreIcon/>
+        <Avatar/>
+      </div>
     </div>
 
   )
